463b1ad735a0bf1be0dc7aedc58a0b47b8817248
Can PERK <canperk@outlook.com>
2022-06-18 11:56:25 +0300

    Set up UI template

--- src/client/admin/src/App.vue
+++ src/client/admin/src/App.vue
@@ -1,21 +1,199 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+      <!-- Menu -->
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+        <div class="app-brand demo">
+          <a href="index.html" class="app-brand-link">
+            <span class="app-brand-logo demo">
+              <img src="/img/logo.svg" />
+            </span>
+            <span class="app-brand-text demo menu-text fw-bolder ms-2">Spread</span>
+          </a>
+
+          <a
+            href="javascript:void(0);"
+            class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none"
+          >
+            <i class="bx bx-chevron-left bx-sm align-middle"></i>
+          </a>
+        </div>
+
+        <div class="menu-inner-shadow"></div>
+
+        <ul class="menu-inner py-1">
+            <!-- Dashboard -->
+            <li class="menu-item active">
+              <a href="index.html" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">Ana Sayfa</div>
+              </a>
+            </li>
+
+            <!-- Layouts -->
+            <li class="menu-item">
+              <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-layout"></i>
+                <div data-i18n="Layouts">Yönetim</div>
+              </a>
+
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="layouts-without-menu.html" class="menu-link">
+                    <div data-i18n="Without menu">Without menu</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="layouts-without-navbar.html" class="menu-link">
+                    <div data-i18n="Without navbar">Without navbar</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="layouts-container.html" class="menu-link">
+                    <div data-i18n="Container">Container</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="layouts-fluid.html" class="menu-link">
+                    <div data-i18n="Fluid">Fluid</div>
+                  </a>
+                </li>
+                <li class="menu-item">
+                  <a href="layouts-blank.html" class="menu-link">
+                    <div data-i18n="Blank">Blank</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            
+           
+          </ul>
+      </aside>
+      <!-- / Menu -->
+
+      <!-- Layout container -->
+      <div class="layout-page">
+        <!-- Navbar -->
+
+        <nav
+          class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+          id="layout-navbar"
+        >
+          <div
+            class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none"
+          >
+            <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+              <i class="bx bx-menu bx-sm"></i>
+            </a>
+          </div>
+
+          <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+            <ul class="navbar-nav flex-row align-items-center ms-auto">
+              <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                <a
+                  class="nav-link dropdown-toggle hide-arrow"
+                  href="javascript:void(0);"
+                  data-bs-toggle="dropdown"
+                >
+                  <div class="avatar avatar-online">
+                    <img src="/img/user.png" class="w-px-40 h-auto rounded-circle" />
+                  </div>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <div class="d-flex">
+                        <div class="flex-shrink-0 me-3">
+                          <div class="avatar avatar-online">
+                            <img
+                              src="/img/user.png"
+                              class="w-px-40 h-auto rounded-circle"
+                            />
+                          </div>
+                        </div>
+                        <div class="flex-grow-1">
+                          <span class="fw-semibold d-block">Esengül Sümeyra</span>
+                          <small class="text-muted">Site Yöneticisi</small>
+                        </div>
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <div class="dropdown-divider"></div>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <i class="bx bx-user me-2"></i>
+                      <span class="align-middle">Hesabım</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <i class="bx bx-cog me-2"></i>
+                      <span class="align-middle">Ayarlar</span>
+                    </a>
+                  </li>
+                  <li>
+                    <div class="dropdown-divider"></div>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="auth-login-basic.html">
+                      <i class="bx bx-power-off me-2"></i>
+                      <span class="align-middle">Çıkış Yap</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <!--/ User -->
+            </ul>
+          </div>
+        </nav>
+
+        <!-- / Navbar -->
+
+        <!-- Content wrapper -->
+        <div class="content-wrapper">
+          <!-- Content -->
+
+          <div class="container-xxl flex-grow-1 container-p-y"></div>
+        </div>
+        <!-- / Content -->
+
+        <!-- Footer -->
+        <footer class="content-footer footer bg-footer-theme">
+          <div
+            class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column"
+          >
+            <div class="mb-2 mb-md-0">
+              © 2022 , made with ❤️ by
+              <a
+                href="https://themeselection.com"
+                target="_blank"
+                class="footer-link fw-bolder"
+                >Boost Team</a
+              >
+            </div>
+            <div>
+              <a
+                href="https://themeselection.com/license/"
+                class="footer-link me-4"
+                target="_blank"
+                >Kullanıcı Sözleşmesi</a
+              >
+            </div>
+          </div>
+        </footer>
+        <!-- / Footer -->
+
+        <div class="content-backdrop fade"></div>
+      </div>
+      <!-- Content wrapper -->
+    </div>
+    <!-- / Layout page -->
+  </div>
+
+  <!-- Overlay -->
+  <div class="layout-overlay layout-menu-toggle"></div>
+</template>
--- src/client/admin/src/main.js
+++ src/client/admin/src/main.js
@@ -1,4 +1,104 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { jQuery, $ } from "./helpers/libs/jquery/jquery";
+import { bootstrap } from "./helpers/root/bootstrap";
+import { Helpers } from "./helpers/root/helpers";
+import { Menu } from "./helpers/root/menu";
 
-createApp(App).mount('#app')
+let menu, animate;
+
+window.Menu = Menu;
+window.Helpers = Helpers;
+
+window.onload = function(){
+  let layoutMenuEl = document.querySelectorAll("#layout-menu");
+  layoutMenuEl.forEach(function (element) {
+    menu = new Menu(element, {
+      orientation: "vertical",
+      closeChildren: false,
+    });
+    Helpers.scrollToActive((animate = false));
+    Helpers.mainMenu = menu;
+  });
+
+  let menuToggler = document.querySelectorAll(".layout-menu-toggle");
+  menuToggler.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+      window.Helpers.toggleCollapsed();
+    });
+  });
+
+  let delay = function (elem, callback) {
+    let timeout = null;
+    elem.onmouseenter = function () {
+      if (!Helpers.isSmallScreen()) {
+        timeout = setTimeout(callback, 300);
+      } else {
+        timeout = setTimeout(callback, 0);
+      }
+    };
+
+    elem.onmouseleave = function () {
+      document.querySelector(".layout-menu-toggle").classList.remove("d-block");
+      clearTimeout(timeout);
+    };
+  };
+  if (document.getElementById("layout-menu")) {
+    delay(document.getElementById("layout-menu"), function () {
+      if (!Helpers.isSmallScreen()) {
+        document.querySelector(".layout-menu-toggle").classList.add("d-block");
+      }
+    });
+  }
+
+  let menuInnerContainer = document.getElementsByClassName("menu-inner"),
+    menuInnerShadow = document.getElementsByClassName("menu-inner-shadow")[0];
+  if (menuInnerContainer.length > 0 && menuInnerShadow) {
+    menuInnerContainer[0].addEventListener("ps-scroll-y", function () {
+      if (this.querySelector(".ps__thumb-y").offsetTop) {
+        menuInnerShadow.style.display = "block";
+      } else {
+        menuInnerShadow.style.display = "none";
+      }
+    });
+  }
+
+  const tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+  const accordionActiveFunction = function (e) {
+    if (e.type == "show.bs.collapse" || e.type == "show.bs.collapse") {
+      e.target.closest(".accordion-item").classList.add("active");
+    } else {
+      e.target.closest(".accordion-item").classList.remove("active");
+    }
+  };
+
+  const accordionTriggerList = [].slice.call(
+    document.querySelectorAll(".accordion")
+  );
+  const accordionList = accordionTriggerList.map(function (accordionTriggerEl) {
+    accordionTriggerEl.addEventListener(
+      "show.bs.collapse",
+      accordionActiveFunction
+    );
+    accordionTriggerEl.addEventListener(
+      "hide.bs.collapse",
+      accordionActiveFunction
+    );
+  });
+
+  Helpers.setAutoUpdate(true);
+  Helpers.initPasswordToggle();
+  Helpers.initSpeechToText();
+
+  if (!Helpers.isSmallScreen()) {
+    Helpers.setCollapsed(true, false);
+  }
+};
+createApp(App).mount("#app");
