fb8ce49512bc96e92f15fdeb5cdeff82ba25ca16
Can PERK <canperk@outlook.com>
2022-07-05 22:48:48 +0300

    Follwer and request components

--- src/client/public/src/components/home/Followers.vue
+++ src/client/public/src/components/home/Followers.vue
@@ -2,26 +2,15 @@
   <div class="widget stick-widget">
     <h4 class="widget-title">Takip İstekleri</h4>
     <ul class="followers">
-      <li>
+      <li v-for="user in users" :key="user.userName">
         <figure>
           <img src="images/resources/side-friend8.jpg" alt="" />
         </figure>
         <div class="friend-meta">
           <h4>
-            <a href="time-line.html" title="">Sophia</a>
+            <a href="time-line.html" title="">{{user.name}}</a>
           </h4>
-          <a href="#" title="" class="underline">Add Friend</a>
-        </div>
-      </li>
-      <li>
-        <figure>
-          <img src="images/resources/side-friend3.jpg" alt="" />
-        </figure>
-        <div class="friend-meta">
-          <h4>
-            <a href="time-line.html" title="">Allen</a>
-          </h4>
-          <a href="#" title="" class="underline">Add Friend</a>
+          <a @click="accept(user.userName)" title="" class="underline">Kabul Et</a>
         </div>
       </li>
     </ul>
@@ -30,5 +19,20 @@
 <script>
 export default {
   name: "Followers",
+  data: () => {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.$ajax.get("api/public/follower/requests").then((response) => {
+      this.users = response.data;
+    });
+  },
+  methods: {
+    accept(userName) {
+      alert(userName);
+    }
+  }
 };
 </script>
--- src/client/public/src/components/home/Friends.vue
+++ src/client/public/src/components/home/Friends.vue
@@ -3,36 +3,19 @@
     <h4 class="widget-title">Arkadaşlar</h4>
     <div id="searchDir"></div>
     <ul id="people-list" class="friendz-list">
-      <li>
+      <li v-for="user in users" :key="user.userName">
         <figure>
           <img src="images/resources/side-friend4.jpg" alt="" />
           <span class="status f-online"></span>
         </figure>
         <div class="friendz-meta">
-          <a href="time-line.html">bucky barnes</a>
+          <a href="time-line.html">{{user.name}}</a>
           <i
             ><a
               href="https://wpkixx.com/cdn-cgi/l/email-protection"
               class="__cf_email__"
               data-cfemail="f2859b9c869780819d9e969780b2959f939b9edc919d9f"
-              >[email&#160;protected]</a
-            ></i
-          >
-        </div>
-      </li>
-      <li>
-        <figure>
-          <img src="images/resources/side-friend2.jpg" alt="" />
-          <span class="status f-away"></span>
-        </figure>
-        <div class="friendz-meta">
-          <a href="time-line.html">Sarah Loren</a>
-          <i
-            ><a
-              href="https://wpkixx.com/cdn-cgi/l/email-protection"
-              class="__cf_email__"
-              data-cfemail="ea888b98848f99aa8d878b8386c4898587"
-              >[email&#160;protected]</a
+              >@{{user.userName}}</a
             ></i
           >
         </div>
@@ -43,5 +26,15 @@
 <script>
 export default {
   name: "Friends",
+  data: () => {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.$ajax.get("api/public/follower/followers").then((response) => {
+      this.users = response.data;
+    });
+  },
 };
 </script>
